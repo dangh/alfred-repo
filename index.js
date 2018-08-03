5ccb55990952fef94b02363dcffc0820060acc49
Dang <huynhminhdang@gmail.com>
2018-08-03 16:30:53 +0800

    cache repo URL for 1 month

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 'use strict';
 const alfy = require('alfy');
 
-alfy
-	.fetch(`https://api.npms.io/v2/package/${alfy.input}`, {
-		json: true,
-		transform: body => body.collected.metadata.links.repository
-	})
-	.then(repoUrl =>
-		alfy.output([
-			{
-				title: `Open ${alfy.input} repo`,
-				subtitle: repoUrl,
-				arg: repoUrl
-			}
-		])
-	);
+const ONE_MONTH = 30 * 24 * 3600 * 1000;
+
+let packageName = alfy.input.toLowerCase();
+
+let showResult = repoUrl => {
+	alfy.output([
+		{
+			title: `Open ${packageName} repo`,
+			subtitle: repoUrl,
+			arg: repoUrl
+		}
+	]);
+};
+
+let repoUrl = alfy.cache.get(packageName);
+if (repoUrl) {
+	showResult(repoUrl);
+} else {
+	alfy
+		.fetch(`https://api.npms.io/v2/package/${packageName}`, {
+			json: true,
+			transform: body => body.collected.metadata.links.repository
+		})
+		.then(repoUrl => {
+			alfy.cache.set(packageName, repoUrl, {maxAge: ONE_MONTH});
+			showResult(repoUrl);
+		});
+}
